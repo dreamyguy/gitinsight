8613f71b08719a33e97be28eae8ca2c1a66b2e04
dreamyguy <i@dreamyguy.com>
2015-10-16 19:42:53 +0200

    Setup 'gulp' and get it to run

---
+++ gulp/index.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+var p = require('gulp-load-plugins')();
+
+p.util.log(
+    'gulp is running'
+);
---
+++ gulpfile.js
@@ -0,0 +1,3 @@
+// Require the gulp setup from one place by running '/gulp/index.js'
+
+require('./gulp');
