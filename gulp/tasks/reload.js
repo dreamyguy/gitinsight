63c1298bdf7cd9953dbe8a5ced15e5245ebd6772
dreamyguy <i@dreamyguy.com>
2015-11-01 21:26:35 +0100

    Introduce 'serve' task

--- gulp/tasks/reload.js
+++ gulp/tasks/reload.js
@@ -3,17 +3,23 @@ var gulp = require('gulp'),
     argv = require('yargs').argv,
     livereload = require('gulp-livereload');
 
+// note that while running the 'gulp serve' task,
+//   the task 'gulp watch --live' (which invokes LiveReload) makes little sense.
+//   'gulp serve', by itself, will reload the page.
+
 // reload livereload
 gulp.task('reload', function() {
+    // only run livereload if arg '--live' is used
     if (argv.live !== undefined) {
         // notify livereload of change
-        util.log('Livereload reported:');
+        util.log('LiveReload reported:');
         livereload.changed('/');
     }
 });
 
 // start livereload server
 gulp.task('reload-start', function() {
+    // only run livereload if arg '--live' is used
     if (argv.live !== undefined) {
         livereload.listen();
     }
