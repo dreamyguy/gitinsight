a5ab9a7fc691de2f1359243f9fd6923973cb4dd9
Wallace Sidhrée <i@dreamyguy.com>
2018-04-08 14:33:18 +0200

    Remove 'karma' and introduce 'jest' as testing framework
    'enzyme' will eventually replace browser tests, but will be introduced later.
    
    Remove 'karma'-related tests, that were only standard generic tests introduced by the boilerplate bootstraper.

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,6 @@ const hub = new HubRegistry([conf.path.tasks('*.js')]);
 gulp.registry(hub);
 
 gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
-gulp.task('test', gulp.series('karma:single-run'));
-gulp.task('test:auto', gulp.series('karma:auto-run'));
 gulp.task('serve', gulp.series('webpack:watch', 'watch', 'browsersync'));
 gulp.task('serve:dist', gulp.series('default', 'browsersync:dist'));
 gulp.task('default', gulp.series('clean', 'move', 'build'));
