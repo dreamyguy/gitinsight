79adfd8916ce89fff362afe68659f3dc3e2bbdb8
Wallace Sidhrée <i@dreamyguy.com>
2020-11-29 14:52:55 +0100

    :zap: Move on to updating 'schema' to match 'gitlogg' formatting, also introducing data to the 'json' server

--- backend/server/graphql/resolvers.js
+++ backend/server/graphql/resolvers.js
@@ -1,5 +1,100 @@
+const axios = require('axios');
+const {
+  PORT_JSON_SERVER,
+  URL_BASE_DEV
+} = require('./../../../config');
+const serverMode = 'json'; // json | something else, eventually
+const graphQLRequestRoot = () => {
+  let url = '';
+  if (serverMode === 'json') {
+    url = `${URL_BASE_DEV}:${PORT_JSON_SERVER}/commits/`;
+  }
+  return url;
+};
+
 const Query = {
-  greeting: () => 'hello'
+  // ('res.data' because 'axios' returns a 'data' array)
+  // all commits
+  commits: () => axios.get(`${graphQLRequestRoot()}`).then(res => res.data),
+  // commits by
+  commitsByRepository: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.repository === args.repository)
+  ),
+  commitsByCommitNr: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.commit_nr === args.commit_nr)
+  ),
+  commitsByCommitHash: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.commit_hash === args.commit_hash)
+  ),
+  commitsByAuthorName: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.author_name === args.author_name)
+  ),
+  commitsByAuthorEmail: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.author_email === args.author_email)
+  ),
+  commitsByAuthorDate: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.author_date === args.author_date)
+  ),
+  commitsByAuthorDateRelative: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.author_date_relative === args.author_date_relative)
+  ),
+  commitsByAuthorDateUnixtimestamp: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.author_date_unix_timestamp === args.author_date_unix_timestamp)
+  ),
+  commitsByAuthorDateIso_8601: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.author_date_iso_8601 === args.author_date_iso_8601)
+  ),
+  commitsBySubject: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.subject === args.subject)
+  ),
+  commitsBySubjectSanitized: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.subject_sanitized === args.subject_sanitized)
+  ),
+  commitsByStats: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.stats === args.stats)
+  ),
+  commitsByTimeHour: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.time_hour === args.time_hour)
+  ),
+  commitsByTimeMinutes: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.time_minutes === args.time_minutes)
+  ),
+  commitsByTimeSeconds: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.time_seconds === args.time_seconds)
+  ),
+  commitsByTimeGmt: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.time_gmt === args.time_gmt)
+  ),
+  commitsByDateDayweek: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.date_day_week === args.date_day_week)
+  ),
+  commitsByDateMonthDay: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.date_month_day === args.date_month_day)
+  ),
+  commitsByDateMonthName: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.date_month_name === args.date_month_name)
+  ),
+  commitsByDateMonthNumber: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.date_month_number === args.date_month_number)
+  ),
+  commitsByDateYear: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.date_year === args.date_year)
+  ),
+  commitsByDateIso8601: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.date_iso_8601 === args.date_iso_8601)
+  ),
+  commitsByFilesChanged: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.files_changed === args.files_changed)
+  ),
+  commitsByInsertions: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.insertions === args.insertions)
+  ),
+  commitsByDeletions: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.deletions === args.deletions)
+  ),
+  commitsByImpact: (parentValue, args) => axios.get(`${graphQLRequestRoot()}`).then(
+    res => res.data.filter(f => f.impact === args.impact)
+  ),
 };
 
 module.exports = { Query };
