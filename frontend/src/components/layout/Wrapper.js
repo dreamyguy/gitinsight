dc072b49d8e4ba5c9551787788721150bff78888
Wallace Sidhrée <i@dreamyguy.com>
2020-12-13 00:17:10 +0100

    :rainbow: Introduce 'gitinsight' colors to the frontend and consolidate them at the config level

--- frontend/src/components/layout/Wrapper.js
+++ frontend/src/components/layout/Wrapper.js
@@ -110,7 +110,7 @@ const SidebarDesktop = () => (
               {/* <!-- Current: "text-gray-300", Default: "text-gray-400 group-hover:text-gray-300" --> */}
               {/* <!-- Heroicon name: home --> */}
               <svg
-                className="text-gray-300 mr-3 h-6 w-6"
+                className="text-fav-orange-middle mr-3 h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -132,7 +132,7 @@ const SidebarDesktop = () => (
             >
               {/* <!-- Heroicon name: users --> */}
               <svg
-                className="text-gray-400 group-hover:text-gray-300 mr-3 h-6 w-6"
+                className="text-fav-orange-dark group-hover:text-gray-300 mr-3 h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -146,7 +146,7 @@ const SidebarDesktop = () => (
                   d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
                 />
               </svg>
-              Team
+              Contributors
             </a>
             <a
               href="#"
@@ -154,7 +154,7 @@ const SidebarDesktop = () => (
             >
               {/* <!-- Heroicon name: folder --> */}
               <svg
-                className="text-gray-400 group-hover:text-gray-300 mr-3 h-6 w-6"
+                className="text-fav-purple-middle group-hover:text-gray-300 mr-3 h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -168,7 +168,29 @@ const SidebarDesktop = () => (
                   d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
                 />
               </svg>
-              Projects
+              Repositories
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+            >
+              {/* <!-- Heroicon name: inbox --> */}
+              <svg
+                className="text-fav-green-dark group-hover:text-gray-300 mr-3 h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+                />
+              </svg>
+              Commits
             </a>
             <a
               href="#"
@@ -176,7 +198,7 @@ const SidebarDesktop = () => (
             >
               {/* <!-- Heroicon name: calendar --> */}
               <svg
-                className="text-gray-400 group-hover:text-gray-300 mr-3 h-6 w-6"
+                className="text-fav-green-light group-hover:text-gray-300 mr-3 h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -196,31 +218,9 @@ const SidebarDesktop = () => (
               href="#"
               className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
             >
-              {/* <!-- Heroicon name: inbox --> */}
-              <svg
-                className="text-gray-400 group-hover:text-gray-300 mr-3 h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                />
-              </svg>
-              Documents
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-            >
               {/* <!-- Heroicon name: chart-bar --> */}
               <svg
-                className="text-gray-400 group-hover:text-gray-300 mr-3 h-6 w-6"
+                className="text-fav-turquoise group-hover:text-gray-300 mr-3 h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -234,7 +234,51 @@ const SidebarDesktop = () => (
                   d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
                 />
               </svg>
-              Reports
+              Trends
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+            >
+              {/* <!-- Heroicon name: chart-bar --> */}
+              <svg
+                className="text-fav-pink-shock group-hover:text-gray-300 mr-3 h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+              Staleness
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+            >
+              {/* <!-- Heroicon name: chart-bar --> */}
+              <svg
+                className="text-fav-yellow group-hover:text-gray-300 mr-3 h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+              Curiosa
             </a>
           </nav>
         </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,33 +4,25 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // Gitinsight logo colors
-        'git-orange-light': '#eb933e',
-        'git-orange-dark': '#d46931',
-        'git-purple': '#7f376d',
-        'git-green-light': '#86b436',
-        'git-green-dark': '#608225',
-        'git-turquoise': '#75fcfd',
-        'git-magenta': '#e84a6c',
-        'git-yellow': '#fbf050',
-
         // Personal favorite colors
         'fav-orange-light': '#fea100',
-        'fav-orangeMiddle': '#f98e1f',
-        'fav-orange-dark': '#e46119',
-        'fav-pinkShock': '#fc3769',
+        'fav-orange-middle': '#f98e1f', // color 1 of gitinsight
+        'fav-orange-dark': '#e46119', // color 2 of gitinsight
+        'fav-pink-shock': '#fc3769', // color 7 of gitinsight
         'fav-purple-light': '#9e4784',
-        'fav-purpleMiddle': '#89326f',
+        'fav-purple-middle': '#89326f', // color 3 of gitinsight
         'fav-purple-dark': '#4d1e40',
         'fav-blue': '#5694f1',
-        'fav-green-light': '#78b600',
-        'fav-green-dark': '#568300',
+        'fav-green-light': '#78b600', // color 5 of gitinsight
+        'fav-green-dark': '#568300', // color 4 of gitinsight
         'fav-beige-light': '#f2f2ea',
         'fav-beige-dark': '#e8e6da',
         'fav-occre-light': '#807067',
-        'fav-occreMiddle': '#6b5b54',
+        'fav-occre-middle': '#6b5b54',
         'fav-occre-dark': '#352f2b',
         'fav-coffee': '#4c3000',
+        'fav-turquoise': '#75fcfd', // color 6 of gitinsight - new
+        'fav-yellow': '#fbf050', // color 8 of gitinsight - new
       },
     },
   },
