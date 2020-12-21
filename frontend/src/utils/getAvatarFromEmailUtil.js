6c62d9bc85d7359bec126ec367e955337c621fa4
Wallace Sidhrée <i@dreamyguy.com>
2020-12-21 01:12:56 +0100

    :eyes: Return better default avatars when 'gravatar' images are not defined

--- frontend/src/utils/getAvatarFromEmailUtil.js
+++ frontend/src/utils/getAvatarFromEmailUtil.js
@@ -1,4 +1,4 @@
 import gravatar from 'gravatar';
 
 // Resolve gravatar url
-export const getAvatarFromEmail = email => gravatar.url(email, { s: '200' });
+export const getAvatarFromEmail = email => gravatar.url(email, { s: '200', d: 'retro' });
