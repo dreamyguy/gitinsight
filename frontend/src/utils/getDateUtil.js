197069b49de27ccbb2a64d0f4e7ac0ee03f34f58
Wallace Sidhrée <i@dreamyguy.com>
2020-12-25 11:36:22 +0100

    :zap: Introduce function that adds missing dates (as they are 0), so that we can render more realistic historical charts

--- frontend/src/utils/getDateUtil.js
+++ frontend/src/utils/getDateUtil.js
@@ -1,3 +1,5 @@
+import { isNotEmptyObject } from './isEmptyUtil';
+
 // Output date based on a valid date value and locale
 export const getDate = (date, locale) => {
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -11,3 +13,53 @@ export const getDate = (date, locale) => {
 // getDate('1382432573', 'no-NB'); // tirsdag 22. oktober 2013
 // getDate('1382432573', 'sr-RS'); // уторак, 22. октобар 2013.
 // getDate('1382432573'); // Tuesday, October 22, 2013
+
+const getAllDaysArray = (start, end) => {
+  const arr = [];
+  for (let dt = new Date(start); dt <= end; dt.setDate(dt.getDate() + 1)) {
+    arr.push(new Intl.DateTimeFormat('fr-CA').format(dt));
+  }
+  return arr;
+};
+
+// Add empty days to the array of days
+export const addEmptyDays = ({ dayList }) => {
+  let finalObj = {};
+  if (dayList && isNotEmptyObject(dayList)) {
+    const daysArray = Object.keys(dayList);
+    const firstDay = daysArray[0];
+    const lastDay = daysArray[daysArray.length - 1];
+    const firstDayDate = new Date(firstDay);
+    const lastDayDate = new Date(lastDay);
+    const allDays = getAllDaysArray(firstDayDate, lastDayDate);
+    allDays.map(ad => {
+      // Key-value pair to be conditionally added to the object
+      const zeroObj = { [ad]: 0 };
+      // If the key is not found in the object, add the key-value pair above
+      if (!Object.keys(dayList).some(s => s === ad)) {
+        finalObj = {
+          ...finalObj,
+          ...zeroObj,
+        };
+      }
+      return null;
+    });
+    // Merge output with original object
+    finalObj = {
+      ...dayList,
+      ...finalObj,
+    };
+    // 'Sort' object by key
+    finalObj = Object.keys(finalObj)
+      .sort()
+      .reduce((acc, cur) => {
+        acc[cur] = finalObj[cur];
+        return acc;
+      }, {});
+  }
+  // Return output
+  return finalObj;
+};
+
+// Usage [see tests]:
+// addEmptyDays({ dayList: days });
--- frontend/src/utils/getDateUtil.test.js
+++ frontend/src/utils/getDateUtil.test.js
@@ -1,20 +1,29 @@
 /* eslint-disable */
-import { getDate } from './getDateUtil';
+import { addEmptyDays, getDate } from './getDateUtil';
+import { addEmptyDaysInput, addEmptyDaysOutput } from './getDateUtil.mock';
 
-describe('getDate', () => {
-  it(`should return expected date from '1382432573' + 'en-US'`, () => {
-    const testInput = getDate('1382432573', 'en-US');
-    const testOutput = 'Tuesday, October 22, 2013';
-    expect(testInput).toEqual(testOutput);
+describe('getDateUtil', () => {
+  describe('getDate', () => {
+    it(`should return expected date from '1382432573' + 'en-US'`, () => {
+      const testInput = getDate('1382432573', 'en-US');
+      const testOutput = 'Tuesday, October 22, 2013';
+      expect(testInput).toEqual(testOutput);
+    });
+    it(`should return expected date from '1382432573'`, () => {
+      const testInput = getDate('1382432573');
+      const testOutput = 'Tuesday, October 22, 2013';
+      expect(testInput).toEqual(testOutput);
+    });
+    // Other examples (not included as tests because Travis doesn't support all these locales):
+    // getDate('1382432573', 'pt-BR'); // terça-feira, 22 de outubro de 2013
+    // getDate('1382432573', 'no-NB'); // tirsdag 22. oktober 2013
+    // getDate('1382432573', 'sr-RS'); // уторак, 22. октобар 2013.
   });
-  it(`should return expected date from '1382432573'`, () => {
-    const testInput = getDate('1382432573');
-    const testOutput = 'Tuesday, October 22, 2013';
-    expect(testInput).toEqual(testOutput);
+  describe('addEmptyDays', () => {
+    it(`should return an object with missing days as { '2012-12-12: 0 }`, () => {
+      const testInput = addEmptyDays({ dayList: addEmptyDaysInput });
+      const testOutput = addEmptyDaysOutput;
+      expect(testInput).toEqual(testOutput);
+    });
   });
 });
-
-// Other examples (not included as tests because Travis doesn't support all these locales):
-// getDate('1382432573', 'pt-BR'); // terça-feira, 22 de outubro de 2013
-// getDate('1382432573', 'no-NB'); // tirsdag 22. oktober 2013
-// getDate('1382432573', 'sr-RS'); // уторак, 22. октобар 2013.
