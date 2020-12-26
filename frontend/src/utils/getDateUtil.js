e4e1ce47b5d73dd831863e81640bed5dcc3dc71f
Wallace Sidhrée <i@dreamyguy.com>
2020-12-26 12:42:41 +0100

    :hourglass: Calculate date through 'dayjs', to avoid problems with 'locale' at runtime

--- frontend/src/utils/getDateUtil.js
+++ frontend/src/utils/getDateUtil.js
@@ -1,23 +1,16 @@
+import dayjs from 'dayjs';
 import { isNotEmptyObject } from './isEmptyUtil';
 
 // Output date based on a valid date value and locale
-export const getDate = (date, locale) => {
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const dtFormat = new Intl.DateTimeFormat(locale || 'en-US', options);
-  return dtFormat.format(date * 1e3);
-};
+export const getDate = date => dayjs.unix(date).format('dddd, MMMM D, YYYY');
 
 // Usage:
-// getDate('1382432573', 'en-US'); // Tuesday, October 22, 2013
-// getDate('1382432573', 'pt-BR'); // terça-feira, 22 de outubro de 2013
-// getDate('1382432573', 'no-NB'); // tirsdag 22. oktober 2013
-// getDate('1382432573', 'sr-RS'); // уторак, 22. октобар 2013.
 // getDate('1382432573'); // Tuesday, October 22, 2013
 
 const getAllDaysArray = (start, end) => {
   const arr = [];
   for (let dt = new Date(start); dt <= end; dt.setDate(dt.getDate() + 1)) {
-    arr.push(new Intl.DateTimeFormat('fr-CA').format(dt));
+    arr.push(dayjs(dt).format('YYYY-MM-DD'));
   }
   return arr;
 };
