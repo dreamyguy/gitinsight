3ea1f9940e09f31142fbb8bc4f127ebe6ad24a59
Wallace Sidhrée <i@dreamyguy.com>
2020-12-28 13:39:32 +0100

    :shower: Cleanup 'curiosa' page

--- frontend/src/components/pages/PageCuriosa.js
+++ frontend/src/components/pages/PageCuriosa.js
@@ -3,11 +3,9 @@ import { useQuery } from '@apollo/react-hooks';
 import { statsGlobalQuery } from '../../graphql/queries';
 import Wrapper from '../layout/Wrapper';
 import PageTitleWithDate from '../content/PageTitleWithDate';
-import CalendarContributions from '../primitives/Calendar/CalendarContributions';
 import Card from '../primitives/Card/Card';
 import Chart from '../primitives/Chart/Chart';
 import { thousandify } from '../../utils/thousandifyUtil';
-import { isNotEmptyObject } from '../../utils/isEmptyUtil';
 
 const PageCuriosa = () => {
   const {
@@ -16,23 +14,10 @@ const PageCuriosa = () => {
       statsGlobal: {
         commitDateFirst,
         commitDateLast,
-        commits,
         commitsImpactGtThousand,
-        commitsOnWeekend,
-        commitsPerContributorAverage,
-        commitsPerDay,
-        commitsPerDayAverage,
         commitsPerYear, // obj with single key-value pair
         commitsWithoutFileChanges,
         commitsWithoutImpact,
-        contributors,
-        daysActive,
-        daysSinceFirstCommit,
-        daysSinceLastCommit,
-        fileChanges,
-        lines,
-        repositories,
-        staleness,
       } = {},
     } = {},
   } = useQuery(statsGlobalQuery);
@@ -42,28 +27,10 @@ const PageCuriosa = () => {
         <>
           <PageTitleWithDate title="Curiosa" from={commitDateFirst} until={commitDateLast} />
           <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-            <Card type="contributors" heading="Contributors" stat={thousandify(contributors)} />
-            <Card type="repositories" heading="Repositories" stat={thousandify(repositories)} />
-            <Card type="code" heading="Commits" stat={thousandify(commits)} />
             <Card
               type="curiosa"
               heading="Commits impact > thousand"
               stat={thousandify(commitsImpactGtThousand)}
-            />
-            <Card
-              type="calendar"
-              heading="Commits on weekends"
-              stat={thousandify(commitsOnWeekend)}
-            />
-            <Card
-              type="trends"
-              heading="Average commits / contributor"
-              stat={commitsPerContributorAverage.toFixed(2)}
-            />
-            <Card
-              type="trends"
-              heading="Average commits / day"
-              stat={commitsPerDayAverage.toFixed(2)}
             />
             <Card
               type="curiosa"
@@ -75,34 +42,7 @@ const PageCuriosa = () => {
               heading="Commits without impact"
               stat={thousandify(commitsWithoutImpact)}
             />
-            <Card
-              type="calendar"
-              heading="Days since first commit"
-              stat={thousandify(daysSinceFirstCommit)}
-            />
-            <Card
-              type="calendar"
-              heading="Days between first and last commit"
-              stat={thousandify(daysActive)}
-            />
-            <Card
-              type="calendar"
-              heading="Days since last commit"
-              stat={thousandify(daysSinceLastCommit)}
-            />
-            <Card type="code" heading="File changes" stat={thousandify(fileChanges)} />
-            <Card type="code" heading="Lines of code" stat={thousandify(lines)} />
-            <Card type="staleness" heading="Staleness" stat={staleness.toFixed(2)} />
           </dl>
-          {commitsPerDay && isNotEmptyObject(commitsPerDay) && (
-            <div className="mt-5">
-              <h2 className="text-2xl font-semibold text-gray-900 mb-5">Commits per day</h2>
-              <CalendarContributions
-                until={Object.keys(commitsPerDay).pop()}
-                values={commitsPerDay}
-              />
-            </div>
-          )}
           <Chart
             categories={Object.keys(commitsPerYear)}
             data={Object.values(commitsPerYear)}
